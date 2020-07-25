97d4fa7182fb5f92998dab472d7cb353d3d323e8
Lil Sizzurp <lilsizzurp@protonmail.com>
2020-07-25 15:21:39 +0200

    add --send param like hastebin

--- index.js
+++ index.js
@@ -2,8 +2,30 @@ const { Plugin } = require('powercord/entities');
 
 module.exports = class Morse extends Plugin {
   startPlugin () {
-    powercord.api.commands.registerCommand({command: 'morse', aliases: [], description: 'Morse-ify text', usage: '{c} [text to morse]', executor: (args) => ({send: true,result: powercord.morse.encode(args.join(" "))})});
-    powercord.api.commands.registerCommand({command: 'demorse', aliases: [], description: 'De-morse-ify text', usage: '{c} [text to morse]', executor: (args) => ({send: true,result: powercord.morse.decode(args.join(" "))})});
+    powercord.api.commands.registerCommand({command: 'morse', aliases: [], description: 'Morse-ify text', usage: '{c} [text to morse]', executor: (args) => (this.handle(args, true))});
+    powercord.api.commands.registerCommand({command: 'demorse', aliases: [], description: 'De-morse-ify text', usage: '{c} [text to morse]', executor: (args) => (this.handle(args, false))});
+  }
+
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('morse');
+    powercord.api.commands.unregisterCommand('demorse');
+
+    powercord.morse = null;
+  }
+
+  handle(args, type) {
+    const send = args.includes('--send') ? !!args.splice(args.indexOf('--send'), 1) : false;
+
+    if(type)
+      return {
+        send,
+        result: powercord.morse.encode(args.join(' '))
+      }
+    else
+      return {
+        send,
+        result: powercord.morse.decode(args.join(' '))
+      }
   }
 };
 
@@ -16,5 +38,5 @@ powercord.morse.encode = (text) => {
 powercord.morse.decode = (text) => {
   var ref = {'.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f','--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l','--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r','...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x','-.--':'y','--..':'z','/':' ','.----':'1','..---':'2','...--':'3','....-':'4','.....':'5','-....':'6','--...':'7','---..':'8','----.':'9','-----':'0'};
   
-  return text.split('    ').map(a => a.split(' ').map(b => ref[b]).join('')).join(' ');
+  return text.split(' ').map(a => a.split(' ').map(b => ref[b]).join('')).join('');
 }
